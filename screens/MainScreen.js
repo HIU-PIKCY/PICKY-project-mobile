9935517ae88b537f6e8c9857fb5ad150344f30fe
오채민 <ocmin20022@gmail.com>
2025-06-14 16:52:25 +0900

    Add: 프로젝트 기본 구조 추가

---
+++ screens/MainScreen.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function MainScreen() {
+  return (
+    <View>
+      <Text>PICKY 메인 화면</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
